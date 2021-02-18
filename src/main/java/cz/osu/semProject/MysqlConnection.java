e3be6e8481652146ad7dde2556925f558979b8df
Radim Staněk <stanek.radim@gtomy.eu>
2021-02-18 13:41:26 +0100

    Sec inicialize

---
+++ src/main/java/cz/osu/semProject/MysqlConnection.java
@@ -0,0 +1,31 @@
+package cz.osu.semProject;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MysqlConnection {
+    private static Connection connection;
+
+    private static final String DB_NAME = "testing";
+    private static final String DB_USERNAME = "test_user";
+    private static final String DB_PASSWORD = "test_password";
+
+    private static void openConnection() throws SQLException {
+        if(connection != null && !connection.isClosed()){
+            return;
+        }
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + DB_NAME + "?useUnicode=yes&characterEncoding=UTF-8", DB_USERNAME, DB_PASSWORD);
+    }
+
+    public static Statement getStatement(){
+        try{
+            openConnection();
+            return connection.createStatement();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Test app");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
